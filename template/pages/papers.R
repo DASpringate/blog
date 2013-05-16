ca9c88600870e54fc10d1da5c9232b7ef8cd61cd
David Springate <daspringate@gmail.com>
2013-05-16 10:13:22 +0100

    Clean up

--- template/pages/papers.R
+++
@@ -1,6 +0,0 @@
-layout <- "default_nocomments.R"
-title <- "papers"
-
-
-page <- content(
-    m("h1", "This page is under construction!"))
--- template/pages/research.R
+++
@@ -1,8 +0,0 @@
-layout <- "default_nocomments.R"
-title <- "research"
-
-research_text <- include.markdown(file.path(site, "template/resources/markdown/research_text.md"))
-
-
-page <- content(m("h1", "My research"),
-              research_text)
